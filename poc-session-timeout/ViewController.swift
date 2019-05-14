dadbf2ee4ca70b85ad7c328f46eefa1bd1f8fddd
Ricardo Yamaguchi <ricardoyamaguchi2011@gmail.com>
2019-05-13 21:09:29 -0300

    Initial Commit

---
+++ poc-session-timeout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  poc-session-timeout
+//
+//  Created by Ricardo Yamaguchi on 13/05/19.
+//  Copyright © 2019 Ricardo Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
